dc48604e109aa2e7f946da5fd2ebd5a37ada20b3
Rimarok <romaric.ruga@gmail.com>
2023-08-04 13:53:02 +0200

    argsReception : log values

--- src/argsReception/argsReception.ts
+++ src/argsReception/argsReception.ts
@@ -5,14 +5,24 @@ export function argsReception() {
     return acc;
   }, {} as { [key: string]: string });
 
+  console.log(process.argv);
+
   const notionToken = args["NOTION_TOKEN"];
   if (!notionToken) throw new Error("NOTION_TOKEN is not defined");
+
   const databaseId = args["DATABASE_ID"];
   if (!databaseId) throw new Error("DATABASE_ID is not defined");
+
+  const cachePath = args["CACHE_PATH"] || "./cache";
+  if (cachePath) console.log(`Cache path override : ${cachePath}`);
+
+  const onOrAfter = args["ON_OR_AFTER"] || undefined;
+  if (onOrAfter) console.log(`Querying overflow : ${onOrAfter}`);
+
   return {
     notionToken,
     databaseId,
-    cachePath: args["CACHE_PATH"] || "./cache",
-    onOrAfter: args["ON_OR_AFTER"] || undefined,
+    cachePath,
+    onOrAfter,
   };
 }
