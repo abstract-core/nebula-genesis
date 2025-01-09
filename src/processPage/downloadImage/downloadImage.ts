33ea6e0108d178a9b119036bb35d1af939fe54c1
Rimarok <romaric.ruga@mailo.fr>
2025-01-09 06:47:37 +0100

    feat : downloadImage resize to 600px max

--- src/processPage/downloadImage/downloadImage.ts
+++ src/processPage/downloadImage/downloadImage.ts
@@ -1,6 +1,7 @@
 import { ImageBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { writeFile } from "fs/promises";
 import fetch from "node-fetch";
+import sharp from "sharp";
 
 export async function downloadImage(
   imagesFolderPath: string,
@@ -14,7 +15,18 @@ export async function downloadImage(
     const res = await fetch(imageUrl);
     const buffer = await res.buffer();
 
-    await writeFile(`${imagesFolderPath}/${filename}`, buffer);
+    // Get image metadata and resize if needed
+    const metadata = await sharp(buffer).metadata();
+
+    const finalBuffer =
+      metadata.width && metadata.width > 600
+        ? await sharp(buffer)
+            .rotate()
+            .resize(600, null, { withoutEnlargement: true })
+            .toBuffer()
+        : buffer;
+
+    await writeFile(`${imagesFolderPath}/${filename}`, finalBuffer);
 
     return filename;
   }
