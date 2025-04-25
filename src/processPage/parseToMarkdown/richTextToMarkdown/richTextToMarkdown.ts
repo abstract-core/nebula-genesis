d67d4b34d1b866418a16d6cb23457da483ab8319
Rimarok <romaric.ruga@mailo.fr>
2025-04-25 18:16:29 +0200

    v2.0.8 : fix : richTextToMarkdown console.log removed

--- src/processPage/parseToMarkdown/richTextToMarkdown/richTextToMarkdown.ts
+++ src/processPage/parseToMarkdown/richTextToMarkdown/richTextToMarkdown.ts
@@ -8,12 +8,9 @@ export function richTextToMarkdown(
   return richTexts
     .map((richText) => {
       if (richText.type === "text") {
-        if (richText.text.link) console.log(richText.text.link);
-        if (richText.href) console.log(richText.href);
         let text = richText.text.content;
         if (richText.href) {
           if (richText.href.startsWith("/1") && pagesSummary) {
-            console.log(pagesSummary[richText.href.slice(1)]);
             const { title, slug } = pagesSummary[richText.href.slice(1)];
             text = `[${title}](${slug})`;
           } else text = `[${text}](${richText.href})`;
